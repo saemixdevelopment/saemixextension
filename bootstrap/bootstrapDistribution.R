7f57c6d48080598d1c9d2d0def9a8707cccd245f
Emmanuelle <emmanuelle.comets@inserm.fr>
2019-07-04 09:57:40 +0200

    Case bootstrap (functions and example code)

---
+++ bootstrap/bootstrapDistribution.R
@@ -0,0 +1,19 @@
+
+saemix.model<-saemix.fit@model
+idx.eps<-saemix.fit@model@indx.res
+idx.iiv<-saemix.fit@model@indx.omega
+
+### Fitting bootstrap - only population parameters and FIM for SE, no log-likelihood
+saemix.bootOpt<-list(fix.seed=F,directory="current",displayProgress=F, save.graphs=F,map=F,ll.is=F)
+
+res.boot<-data.frame()
+for(iboot in 1:nboot) {
+  data.bootCase <- dataGen.case(saemix.fit)
+  fit.bootCase<-saemix(saemix.model,data.bootCase,saemix.bootOpt)
+  res<-fit.bootCase@results
+  l1<-c(iboot,res@fixed.effects, diag(res@omega)[idx.iiv],res@respar[idx.eps], res@se.fixed, res@se.omega[idx.iiv], res@se.respar[idx.eps],res@ll.lin)
+  res.boot<-rbind(res.boot,l1) 
+}
+lnam<-c(saemix.model@name.fixed,saemix.model@name.random,saemix.model@name.sigma[idx.eps])
+colnames(res.boot)<-c("Replicate",lnam, paste("SE",lnam,sep="."),"LL")
+
