e5dd0d304c00e596e14b75d26887f420c1db740c
Johannes Ranke <jranke@uni-bremen.de>
2021-10-27 05:38:42 +0200

    Update tests and make them less chatty
    - The data() command in the setup script is necessary for running
      tests using devtools::test() because of its nonstandard evaluation
    - Likelihoods calculated per default have slightly changed
    - compare.saemix currently gives a warning instead of an error if the
    likelihood calculated by the requested method is not available
    - Avoid output of plots and fitting results to facilitate focussing on
    the test results

--- tests/testthat/test-logLik.R
+++ tests/testthat/test-logLik.R
@@ -4,23 +4,23 @@ test_that("Log-Likelihood, AIC and BIC can be extracted", {
 
   ll_is <- logLik(theo_fit)
   expect_equal(class(ll_is), "logLik")
-  expect_equal(round(as.numeric(ll_is), 2), -172.81)
+  expect_equal(round(as.numeric(ll_is), 2), -172.80)
   expect_equal(attr(ll_is, "df"), 7)
 
   AIC_is <- AIC(theo_fit)
-  expect_equal(round(AIC_is, 2), 359.63)
+  expect_equal(round(AIC_is, 2), 359.60)
 
   BIC_is <- BIC(theo_fit)
-  expect_equal(round(BIC_is, 2), 363.02)
+  expect_equal(round(BIC_is, 2), 362.99)
 
   ll_lin <- logLik(theo_fit, method = "lin")
-  expect_equal(round(as.numeric(ll_lin), 2), -171.98)
+  expect_equal(round(as.numeric(ll_lin), 2), -171.96)
 
   AIC_lin <- AIC(theo_fit, method = "lin")
-  expect_equal(round(AIC_lin, 2), 357.96)
+  expect_equal(round(AIC_lin, 2), 357.92)
 
   BIC_lin <- BIC(theo_fit, method = "lin")
-  expect_equal(round(BIC_lin, 2), 361.35)
+  expect_equal(round(BIC_lin, 2), 361.32)
 
   expect_error(ll_gq <- logLik(theo_fit, method = "gq"), "not yet.*computed")
 })
@@ -49,11 +49,11 @@ test_that("Model comparison works as expected", {
   expect_true(all(comp_2[1, "BIC.cov"] < comp_2[2:4, "BIC.cov"]))
 
   # Gaussian quadrature also gives the same orders
-  expect_error(compare.saemix(theo_fit, theo_fit_1_cov, method = "gq"),
+  expect_warning(compare.saemix(theo_fit, theo_fit_1_cov, method = "gq"),
     "not available for model 1")
   theo_fit_gq <- llgq.saemix(theo_fit)
 
-  expect_error(compare.saemix(theo_fit_gq, theo_fit_1_cov, method = "gq"),
+  expect_warning(compare.saemix(theo_fit_gq, theo_fit_1_cov, method = "gq"),
     "not available for model 2")
 
   theo_fit_1_cov_gq <- llgq.saemix(theo_fit_1_cov)
