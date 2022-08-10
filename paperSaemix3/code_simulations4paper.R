e74ee5a05b35ea9d6b2048870e6f5c77bc8b1bc1
Emmanuelle <emmanuelle.comets@inserm.fr>
2022-08-10 16:02:02 +0200

    Saemix 3 paper - notebooks for the different examples in folder paperSaemix3

---
+++ paperSaemix3/code_simulations4paper.R
@@ -0,0 +1,14 @@
+# Directories
+saemixDir<-"/home/eco/work/saemix/saemixextension"
+datDir<-file.path(saemixDir,"data")
+
+# library saemix - currently (August 2022) saemix 3.1
+library(saemix)
+
+# Other libraries
+library(ggplot2)
+library(MASS)
+library(rlang)
+library(gridExtra)
+library(tidyverse)
+
