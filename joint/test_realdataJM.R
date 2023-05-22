3c31730e7208725c2ae36a1b2b057ef09feb983a
alexandralavmo <99263619+alexandralavmo@users.noreply.github.com>
2023-05-22 10:17:04 +0200

    changes saemix paper code
    changes about test_realdataJM.R

--- joint/test_realdataJM.R
+++ joint/test_realdataJM.R
@@ -55,13 +55,6 @@ dataJM<-saemixData(name.data=data_joint, name.group=c("id"), name.predictors=c("
                    name.response="obs",name.ytype = "ytype")
 
 
-
-#### initializing parameters
-
-param<-c(73,1.25,0.6,0.0001)
-omega.sim<-c(18, 3, 0.05, 0.01)
-sigma.sim <- 17
-
 # Model definition 
 
 JMmodel<-function(psi,id,xidep) {
@@ -95,6 +88,14 @@ JMmodel<-function(psi,id,xidep) {
 }
 
 
+#### initializing parameters
+
+param<-c(73,1.25,0.6,0.0001)
+omega.sim<-c(18, 3, 0.05, 0.01)
+sigma.sim <- 17
+
+### saemix Model 
+
 jointTTE<-saemixModel(model=JMmodel,description="JM LMEM-TTE (prothro data)",modeltype=c("structural","likelihood"),
                       psi0=matrix(param,ncol=4,byrow=TRUE,dimnames=list(NULL, c("b0","b1","h0","alpha"))),
                       transform.par=c(0,0,1,0), covariance.model=diag(c(1,1,0,0)),
@@ -110,14 +111,127 @@ summary(yfit) # parameter estimates + likelihood
 yfit@results@fim # inverse of the Fisher Information Matrix 
 sqrt(diag(yfit@results@fim)) # SE of parameter estimates 
 
+## Other examples 
 
-################### WITH a nonlinear mixed-effects model ###################
+######### Model 2: with a covariate on slope parameter (treatment: placebo or prednisone) ##############
+
+# Formatting and creating data 
+d1 = prothro[,c(1,2,3,4)]
+d1$ytype=1
+colnames(d1)[2] = "obs"
+colnames(d1)[4] = "cov"
+d2 = prothros[,c(1,3,2,4)]
+d2$ytype = 2
+colnames(d2)[2] = "obs"
+colnames(d2)[3] = "time"
+colnames(d2)[4] = "cov"
+data_joint = rbind(d1,d2)
+dataJM<-saemixData(name.data=data_joint, name.group=c("id"), name.predictors=c("time","obs"),
+                   name.response="obs",name.ytype = "ytype", name.covariates = "cov")
+
+jointTTE<-saemixModel(model=JMmodel,description="JM LMEM-TTE (prothro data)",modeltype=c("structural","likelihood"),
+                      psi0=matrix(param,ncol=4,byrow=TRUE,dimnames=list(NULL, c("b0","b1","h0","alpha"))),
+                      transform.par=c(0,0,1,0), covariance.model=diag(c(1,1,0,0)),
+                      fixed.estim = c(1,1,1,1),error.model = "constant",
+                      omega.init = diag(omega.sim), covariate.model = c(0,1,0,0))
+
+saemix.data<-dataJM
+saemix.model<-jointTTE
+saemix.options<-saemixControl(seed=12345, map=T, fim=T, ll.is=TRUE, save.graphs = F) # please, specify save.graphs=F (currently not extended)
+yfit <- saemix.multi(saemix.model, saemix.data, saemix.options)
+
+yfit@results@fim # inverse of the Fisher Information Matrix 
+sqrt(diag(yfit@results@fim)) # SE of parameter estimates 
+
+
+
+######### Model 3: with a covariate on the survival model using a dummy variable (treatment: placebo or prednisone) ##############
+
+# Formatting and creating data 
+d1 = prothro[,c(1,2,3,4)]
+d1$ytype=1
+colnames(d1)[2] = "obs"
+colnames(d1)[4] = "cov"
+d2 = prothros[,c(1,3,2,4)]
+d2$ytype = 2
+colnames(d2)[2] = "obs"
+colnames(d2)[3] = "time"
+colnames(d2)[4] = "cov"
+data_joint = rbind(d1,d2)
+dataJM<-saemixData(name.data=data_joint, name.group=c("id"), name.predictors=c("time","obs"),
+                   name.response="obs",name.ytype = "ytype", name.covariates = "cov")
+
+
+
+JMmodel<-function(psi,id,xidep) {
+  ytype<-xidep$ytype  # type of response (1: continuous, 2: event)
+  b0 <- psi[id,1] 
+  b1 <- psi[id,2] 
+  h0 <- psi[id,3]
+  alpha <- psi[id,4] 
+  trt <- psi[id,5]
+  
+  ypred <- b0+b1*xidep[,1]   
+  
+  T<-xidep[ytype==2,1]# vector of times (survival part)
+  Nj <- length(T)
+  ev = xidep$obs[ytype==2]
+  cens<-which(ev==0) 
+  ind <- which(ev==1)
+  b0b = b0[ytype==2] # to have vectors of the same length as T 
+  b1b = b1[ytype==2]
+  h0b = h0[ytype==2]
+  alphab = alpha[ytype==2]
+  
+  haz <- h0b*exp(alphab*(b0b+b1b*T)+trt) # instantaneous hazard
+  H <- (h0b/(alphab*b1b))*exp(alphab*(b0b+b1b*T)+trt)-(h0b/(alphab*b1b))*exp(alphab*b0b+trt) # cumulative hazard
+  
+  logpdf <- rep(0,Nj)
+  logpdf[cens] <- -H[cens] 
+  logpdf[ind] <- -H[ind] + log(haz[ind]) 
+  
+  ypred[ytype==2] = logpdf
+  return(ypred)
+}
+
+# initializing parameters 
+param<-c(73,1.25,0.6,0.0001,0)
+omega.sim<-c(18, 3, 0.05, 0.01, 0.01)
+sigma.sim <- 17
+
+jointTTE<-saemixModel(model=JMmodel,description="JM LMEM-TTE (prothro data)",modeltype=c("structural","likelihood"),
+                      psi0=matrix(param,ncol=5,byrow=TRUE,dimnames=list(NULL, c("b0","b1","h0","alpha","trt"))),
+                      transform.par=c(0,0,1,0,0), covariance.model=diag(c(1,1,0,0,0)),
+                      fixed.estim = c(1,1,1,1,0),error.model = "constant",
+                      omega.init = diag(omega.sim),covariate.model = c(0,0,0,0,1))
+
+
+saemix.data<-dataJM
+saemix.model<-jointTTE
+saemix.options<-saemixControl(seed=12345, map=T, fim=T, ll.is=TRUE, save.graphs = F) # please, specify save.graphs=F (currently not extended)
+yfit <- saemix.multi(saemix.model, saemix.data, saemix.options)
+
+yfit@results@fim # inverse of the Fisher Information Matrix 
+sqrt(diag(yfit@results@fim)) # SE of parameter estimates 
+
+
+
+
+######### Model 4: with a nonlinear mixed-effects model ##########
 # time consuming because the model as no explicit likelihood expression...
 # use of numerical integration to compute joint likelihood 
 
-param<-c(20,2,0.6,0.5,2,-0.039)
-omega.sim<-c(5, 1, 0.6,0.5,0.02, 0.03)
-sigma.sim <- 17
+# Formatting and creating data 
+d1 = prothro[,c(1,2,3)]
+d1$ytype=1
+colnames(d1)[2] = "obs"
+d2 = prothros[,c(1,3,2)]
+d2$ytype = 2
+colnames(d2)[2] = "obs"
+colnames(d2)[3] = "time"
+data_joint = rbind(d1,d2)
+dataJM<-saemixData(name.data=data_joint, name.group=c("id"), name.predictors=c("time","obs"),
+                   name.response="obs",name.ytype = "ytype")
 
 # model
 JMmodel_nl<-function(psi,id,xidep) {
@@ -159,6 +273,10 @@ JMmodel_nl<-function(psi,id,xidep) {
   ypred[ytype==2] = logpdf
   return(ypred)
 }
+
+param<-c(20,2,0.6,0.5,2,-0.039)
+omega.sim<-c(5, 1, 0.6,0.5,0.02, 0.03)
+sigma.sim <- 17
 
 jointTTE_nl<-saemixModel(model=JMmodel_nl,description="JM lin longi one tte",modeltype=c("structural","likelihood"),
                       psi0=matrix(param,ncol=6,byrow=TRUE,dimnames=list(NULL, c("b0","b1","b2","a","h0","alpha"))),
